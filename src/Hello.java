f68af0936d06561c3c0a79a6aca56fea0ce6bb99
Rick Blumenthal <rblument@regis.edu>
2015-04-11 16:01:42 -0600

    Third

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * tIBIS.java (1.0) 6/02/08
+ *
+ * CtxIBIS: Contextualized IBIS (Issue-Based Information System)
+ * 
+ * (C) 2008-2013 Rick Blumenthal.  All rights reserved.
+ * Unauthorized duplication/distribution violates copyright law.
+ */
+package edu.regis.tibis;
+
+public class Hello {
+    public static void main(String[] args) {
+	System.out.println("hello, world");
+    }
+}
